11162bc08ba572dc52803a5289f9f21c2501cdf2
R. Coder <77580222+Esteklo@users.noreply.github.com>
2021-09-04 00:37:29 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # import os
-# SECRET_KEY = os.urandom(32)
+SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
-# basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
-# DEBUG = True
+DEBUG = True
 
 # Connect to the database
 
