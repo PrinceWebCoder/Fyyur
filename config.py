098bf2fc21c2608cc4d95d90e358abb9c1da300b
R. Coder <77580222+Esteklo@users.noreply.github.com>
2021-09-04 00:35:21 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
-import os
-SECRET_KEY = os.urandom(32)
+# import os
+# SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
-basedir = os.path.abspath(os.path.dirname(__file__))
+# basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
-DEBUG = True
+# DEBUG = True
 
 # Connect to the database
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgres://hwabivydkzlskf:0ac8a4cbb275bf218592e40b22dae4d2693618d74b63e93fc9761e461b6f61d6@ec2-44-198-80-194.compute-1.amazonaws.com:5432/db2g12mkfigind'
+# SQLALCHEMY_DATABASE_URI = 'postgres://hwabivydkzlskf:0ac8a4cbb275bf218592e40b22dae4d2693618d74b63e93fc9761e461b6f61d6@ec2-44-198-80-194.compute-1.amazonaws.com:5432/db2g12mkfigind'
