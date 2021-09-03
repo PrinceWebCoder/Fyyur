377a23effd171fcab106f75e37fb18db23fed305
R. Coder <77580222+Esteklo@users.noreply.github.com>
2021-09-04 00:59:10 +0500

    Update models.py

--- models.py
+++ models.py
@@ -12,6 +12,11 @@ def setup_db(app, database_path=database_path):
     db.app = app
     db.init_app(app)
 
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
+
+
 
 class Venue(db.Model):
     __tablename__ = 'venue'
