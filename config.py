0af215baf3d29854bc319053d8ee1ed2455c3a69
R. Coder <77580222+Esteklo@users.noreply.github.com>
2021-09-03 18:54:39 +0500

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,9 @@ DEBUG = True
 
 # Connect to the database
 
+database_filename = "database.db"
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URI = database_path
