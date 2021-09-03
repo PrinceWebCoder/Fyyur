37dac30e33bdef2621bc9db66c96ea189cd2d622
R. Coder <77580222+Esteklo@users.noreply.github.com>
2021-09-04 01:10:32 +0500

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 
 
-database_path = 'postgres://hwabivydkzlskf:0ac8a4cbb275bf218592e40b22dae4d2693618d74b63e93fc9761e461b6f61d6@ec2-44-198-80-194.compute-1.amazonaws.com:5432/db2g12mkfigind'
+database_path = 'postgresql://hwabivydkzlskf:0ac8a4cbb275bf218592e40b22dae4d2693618d74b63e93fc9761e461b6f61d6@ec2-44-198-80-194.compute-1.amazonaws.com:5432/db2g12mkfigind'
 
 
 db = SQLAlchemy()
